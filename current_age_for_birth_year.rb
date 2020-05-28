843016482fbc97748d29fc94a96b2c6c8a674836
Patrick Pierre <patrick.pierre000@gmail.com>
2020-05-28 01:15:58 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,6 +1,5 @@
 def current_age_for_birth_year(birth_year)
-  age_now = Time.now.year - birth_year
-  puts age_now
-  return age_now
+  Time.now.year - birth_year
+ 
 end
 current_age_for_birth_year(2001)
